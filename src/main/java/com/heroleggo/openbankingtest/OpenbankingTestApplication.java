1908bf798f8fab3fda11d2eeae37290dad8b4e06
Youngwoong You <h3r0.y0u@gmail.com>
2023-04-27 13:35:14 +0900

    init: Initialize project

---
+++ src/main/java/com/heroleggo/openbankingtest/OpenbankingTestApplication.java
@@ -0,0 +1,13 @@
+package com.heroleggo.openbankingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankingTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OpenbankingTestApplication.class, args);
+  }
+
+}
